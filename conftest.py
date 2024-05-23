492b7220653d0851f792bb3ac9932b835dc21ad6
Pamella Bezerra <pamella@vinta.com.br>
2024-05-23 14:52:33 -0300

    Add missing lint setting

--- conftest.py
+++ conftest.py
@@ -5,7 +5,6 @@ from django.conf import settings
 
 import pytest
 
-
 # We manually designate which settings we will be using in an environment variable
 # This is similar to what occurs in the `manage.py`
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "test_settings")
